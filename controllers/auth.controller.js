a6b7a06545eab7a6f5fb61e3c3ea03b1522bff12
Vivek <vkc8181@gmail.com>
2022-05-08 15:31:52 +0530

    Added validation message generator while signing up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,38 @@
 const User = require('../models/user.model');
 
+function generateErrorObject(err) {
+    const errorObj = {
+        username: [],
+        password: []
+    }
+
+    if(err.message.includes('duplicate key error collection')){
+        errorObj.username.push('Username already taken');
+    }
+    if(err.message.includes('Username too short')){
+        errorObj.username.push('Username too short');
+    }
+    if(err.message.includes('Username Required')){
+        errorObj.username.push('Username Required');
+    }
+
+    if(err.message.includes('Password too short')){
+        errorObj.password.push('Password too short');
+    }
+    if(err.message.includes('Password Required')){
+        errorObj.password.push('Password Required');
+    }
+
+    return errorObj;
+
+}
+
 function handleSignupGet(req, res) {
     res.render('signup.ejs');
 }
 
 async function handleSignupPost(req, res) {
+
     const { username, password } = req.body;
 
     try{
@@ -12,8 +40,8 @@ async function handleSignupPost(req, res) {
         res.status(201).json(user);
     }
     catch(e) {
-        res.status(400).send('Someting went wrong');
-        console.log(e);
+        res.status(400).json( generateErrorObject(e) );
+        console.log(e.message);
     }
 
 }
--- models/user.model.js
+++ models/user.model.js
@@ -3,14 +3,15 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        unique: true,
-        required: true,
-        lowercase: true, 
+        unique: [true, 'Username taken'],   //Not giving this error message (idk why)
+        minlength: [3, 'Username too short'],
+        required: [true, 'Username Required'],
+        lowercase: true 
     },
     password: {
         type: String,
-        required: true,
-        minlength: 6
+        required: [true, 'Password Required'],
+        minlength: [6, 'Password too short']
     }
 });
 
