749ebc4126fbce8d88ec89439d9a2e57468b747c
Vivek <vkc8181@gmail.com>
2022-05-09 11:33:13 +0530

    Password being hashed before storing

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -13,6 +14,29 @@ const userSchema = new mongoose.Schema({
         required: [true, 'Password Required'],
         minlength: [6, 'Password too short']
     }
+});
+
+// DB HOOK - This is how they work
+/*
+//Gets fired before the document is saved
+userSchema.pre('save', function(next) {
+    console.log('Data about to be saved!!!');
+    console.log(this);
+    next();
+});
+
+//Gets fired after the document is saved
+userSchema.post('save', function(doc, next) {   //doc is the saved object which is equivalant to "this" here
+    console.log('Data saved sussessfully!!!');
+    console.log(doc);
+    next();
+});
+*/
+
+userSchema.pre('save', async function(next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
